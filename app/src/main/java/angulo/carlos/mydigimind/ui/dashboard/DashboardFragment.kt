cd0bd3f1ab39019b53f14469506ac56ba482dfc9
CarlosAngulo30 <carlos.angulo228410@potros.itson.edu.mx>
2023-03-01 22:45:53 -0700

    Práctica 8 parte 2. Digimind

--- app/src/main/java/angulo/carlos/mydigimind/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/angulo/carlos/mydigimind/ui/dashboard/DashboardFragment.kt
@@ -1,13 +1,17 @@
 package angulo.carlos.mydigimind.ui.dashboard
 
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import angulo.carlos.mydigimind.R
 import angulo.carlos.mydigimind.databinding.FragmentDashboardBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class DashboardFragment : Fragment() {
 
@@ -28,9 +32,16 @@ class DashboardFragment : Fragment() {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
+        val btn_time: Button = root.findViewById(R.id.btn_time)
+        btn_time.setOnClickListener{
+            val cal = Calendar.getInstance()
+            val timeSetListener = TimePickerDialog.OnTimeSetListener { timePicker, hour, minute ->
+                cal.set(Calendar.HOUR_OF_DAY,hour)
+                cal.set(Calendar.MINUTE, minute)
+
+                btn_time.text = SimpleDateFormat("HH:mm").format(cal.time)
+            }
+            TimePickerDialog(root.context, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
         }
         return root
     }
